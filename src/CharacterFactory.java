c3dc9d8e99c0a1e8230650769b82f67a1997368d
‘tekuprogramming’ <mila.p.06@seznam.cz>
2024-03-25 10:01:57 +0100

    edited createCharacter added method applyRaceBonuses

--- src/CharacterFactory.java
+++ src/CharacterFactory.java
@@ -1,5 +1,13 @@
 public class CharacterFactory {
-    public static Character createCharacter(String name, int level) {
-        return new Character(name, level);
+    public static Character createCharacter(String name, String race, String characterClass, int level) {
+        Character character = new Character(name, level);
+        return character;
+    }
+
+    public static void applyRaceBonuses(Character character, String race) {
+        switch (race) {
+            case "human":
+
+        }
     }
 }
